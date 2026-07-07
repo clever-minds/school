ae4e7cd5bd9846348a2b19dc4b7ace9e5cca562f
ujjawal <ujjawal.infonex@gmail.com>
2026-07-07 14:08:02 +0530

    add JIAM address and logo

--- resources/views/students/students_id_card.blade.php
+++ resources/views/students/students_id_card.blade.php
@@ -31,25 +31,23 @@ body {
 .logo-wrapper {
     position: absolute;
     left: 50%;
+    top: 15px;
     transform: translateX(-50%);
+    width: 100%;
 }
 
 .logo-box {
-    width: auto;
-    height: 80px;
+    width: 100%;
+    height: 100px;
     display: flex;
     justify-content: center;
     align-items: center;
-    border-radius: 6px;
     overflow: hidden;
-    padding-top: 18%;
-
-
 }
 
 .logo-box img {
     width: auto;
-    height: 75px;
+    height: 95px;
     object-fit: contain;
 }
 
