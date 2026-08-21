63c6320770cc7a98b384fd0c07f064bbf24ece64
ujjawal <ujjawal.infonex@gmail.com>
2026-08-21 11:30:09 +0530

    Remove Optional Fees column from fees paid list

--- resources/views/fees/fees_paid.blade.php
+++ resources/views/fees/fees_paid.blade.php
@@ -134,7 +134,7 @@
                                     <th scope="col" data-field="full_name" data-sortable="false" data-align="center"> {{ __('Student Name') }}</th>
                                     <th scope="col" data-field="student.class_section.full_name" data-sortable="false" data-align="center">{{ __('Class') }}</th>
                                     <th scope="col" data-field="total_compulsory_fees" data-sortable="false" data-formatter="amountFormatter" data-align="center">{{ __('Compulsory Fees') }}</th>
-                                    <th scope="col" data-field="total_optional_fees" data-sortable="false" data-formatter="amountFormatter" data-align="center">{{ __('Optional Fees') }}</th>
+
                                     <th scope="col" data-field="paid_amount" data-sortable="false" data-formatter="amountFormatter" data-align="center">{{ __('Paid Amount') }}</th>
                                     <th scope="col" data-field="payment_method" data-sortable="false" data-align="center"> {{ __('Payment Method') }}</th>
                                     <th scope="col" data-field="fees_status" data-sortable="false" data-formatter="feesPaidStatusFormatter" data-align="center"> {{ __('Fees Status') }}</th>
